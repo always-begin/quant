17685d469f6ade511960352404c1d464f309e42e
always-begin <bysu.park@gmail.com>
2023-03-12 18:36:23 +0900

    feat : change evaluate method

--- method.py
+++ method.py
@@ -19,32 +19,32 @@ def get_yf_df(name, startDt, endDt):
 # %% set strategy
 
 
-def get_trend_of_macd_df(input_df, short=12, long=26, signal=9, price_field='Close'):
+def get_trend_of_macd_df(input_df, short=12, long=26, signal=9, factor='Close'):
 
   output_df = input_df.copy()
-  output_df['ema_short'] = input_df[price_field].ewm(span=short).mean()
-  output_df['ema_long'] = input_df[price_field].ewm(span=long).mean()
+  output_df['ema_short'] = input_df[factor].ewm(span=short).mean()
+  output_df['ema_long'] = input_df[factor].ewm(span=long).mean()
   output_df['macd'] = (output_df['ema_short']-output_df['ema_long']).round(2)
   output_df['macd_signal'] = output_df['macd'].ewm(span=signal).mean().round(2)
   output_df['macd_oscillator'] = (output_df['macd']-output_df['macd_signal']).round(2)
-  ret = output_df[[price_field, 'macd', 'macd_signal', 'macd_oscillator']]
+  ret = output_df[[factor, 'macd', 'macd_signal', 'macd_oscillator']]
   print(f"macd output_df : {ret.columns}")
   return ret
 
 
-def get_trend_of_rsi(input_df, window=14, price_field='Close'):
+def get_trend_of_rsi(input_df, window=14, factor='Close'):
 
   output_df = input_df.copy()
   output_df.fillna(method='ffill', inplace=True)
   if len(output_df) > window:
-    output_df['diff'] = output_df.loc[:, price_field].diff()
+    output_df['diff'] = output_df.loc[:, factor].diff()
     output_df['au'] = output_df['diff'].where(output_df['diff'] > 0, 0).rolling(window).mean()
     output_df['ad'] = output_df['diff'].where(output_df['diff'] < 0, 0).rolling(window).mean().abs()
     for r in range(window+1, len(output_df)):
       output_df['au'][r] = (output_df['au'][r-1]*(window-1) + output_df['diff'].where(output_df['diff'] > 0, 0)[r]) / window
       output_df['ad'][r] = (output_df['ad'][r-1]*(window-1) + output_df['diff'].where(output_df['diff'] < 0, 0).abs()[r]) / window
     output_df['rsi'] = (output_df['au'] / (output_df['au'] + output_df['ad']) * 100).round(2)
-    ret = output_df[[price_field, 'rsi']]
+    ret = output_df[[factor, 'rsi']]
     print(f"rsi output_df : {ret.columns}")
     return ret
   else:
@@ -52,13 +52,22 @@ def get_trend_of_rsi(input_df, window=14, price_field='Close'):
     return None
   
 
-def get_trend_of_envelope(input_df, window=20, spread = .05, price_field='Close'):
+def get_trend_of_envelope(input_df, window=20, spread = .05, factor='Close'):
 
   output_df = input_df.copy()
-  output_df['center'] = output_df[price_field].rolling(window).mean()
+  output_df['center'] = output_df[factor].rolling(window).mean()
   output_df['ub'] = output_df['center']*(1+spread)
   output_df['lb'] = output_df['center']*(1-spread)
-  return output_df[[price_field, 'center', 'ub', 'lb']]
+  return output_df[[factor, 'center', 'ub', 'lb']]
+
+
+def get_trend_of_bollinger(input_df, w=20, k=2, factor='Close'):
+  output_df = input_df.copy()
+  output_df['center'] = output_df[factor].rolling(w).mean()
+  output_df['sigma'] = output_df[factor].rolling(w).std()
+  output_df['ub'] = output_df['center'] + k * output_df['sigma']
+  output_df['lb'] = output_df['center'] - k * output_df['sigma']
+  return output_df[[factor, 'center','ub','lb']]
 
 
 # %% set position
@@ -103,6 +112,31 @@ def add_band_to_signal(df, factor, buy, sell):
     df['trade'].fillna('zero', inplace=True)
     return df['trade']
 
+def add_combine_signal_and(df, conditions):
+    '''
+    Combine signals as intersection
+    :param df: Dataframe containing historical prices
+    :param cond: Columns to be combined
+    :return: Dataframe of selected signals
+    '''
+    for c in conditions:
+        df['trade'].mask((df['trade'] == 'have') & (df[c] == 'have'), 'have', inplace=True)
+        df['trade'].mask((df['trade'] == 'zero') | (df[c] == 'zero'), 'zero', inplace=True)
+    return df
+
+
+def add_combine_signal_or(df, conditions):
+    '''
+    Combine signals as union
+    :param df: Dataframe containing historical prices
+    :param cond: Columns to be combined
+    :return: Dataframe of selected signals
+    '''
+    for c in conditions:
+        df['trade'].mask((df['trade'] == 'have') | (df[c] == 'have'), 'have', inplace=True)
+        df['trade'].mask((df['trade'] == 'zero') & (df[c] == 'zero'), 'zero', inplace=True)
+    return df
+
 
 def add_position_df(df):
   pos = 'position'
@@ -121,7 +155,7 @@ def add_position_df(df):
 # %% to evaluate a strategy
 
 
-def get_evaluate_column(df, price_field='Close', cost=.001, rf_rate=.01):
+def get_evaluate_column(df, factor='Close', cost=.001, rf_rate=.01,isPrint=True):
   '''
   Calculate daily returns and MDDs of portfolio
   :param df: The dataframe containing trading position
@@ -130,15 +164,15 @@ def get_evaluate_column(df, price_field='Close', cost=.001, rf_rate=.01):
   '''
   df = df.copy()
   df['signal_price'] = np.nan
-  df['signal_price'].mask(df['position'] == 'buy', df.loc[:, price_field], inplace=True)
-  df['signal_price'].mask(df['position'] == 'sell', df.loc[:, price_field], inplace=True)
+  df['signal_price'].mask(df['position'] == 'buy', df.loc[:, factor], inplace=True)
+  df['signal_price'].mask(df['position'] == 'sell', df.loc[:, factor], inplace=True)
 
   record = df[['position', 'signal_price']].dropna()
   record['rtn'] = 1
   record['rtn'].mask(record['position'] == 'sell', (record['signal_price'] * (1-cost))/record['signal_price'].shift(1), inplace=True)
   record['acc_rtn'] = record['rtn'].cumprod()
 
-  df['signal_price'].mask(df['position'] == 'have_wait', df.loc[:, price_field], inplace=True)
+  df['signal_price'].mask(df['position'] == 'have_wait', df.loc[:, factor], inplace=True)
   df['rtn'] = record['rtn']
   df['rtn'].fillna(1, inplace=True)
 
@@ -152,12 +186,12 @@ def get_evaluate_column(df, price_field='Close', cost=.001, rf_rate=.01):
   df['mdd'] = (df['acc_rtn'] / df['acc_rtn'].cummax()).round(4)
   df['bm_mdd'] = (df.iloc[:, 0] / df.iloc[:, 0].cummax()).round(4)
   df.drop(columns='signal_price', inplace=True)
-  calcualte_performance(df, rf_rate)
+  ret = calcualte_performance(df, rf_rate, isPrint)
 
-  return df
+  return df, ret
 
 
-def calcualte_performance(df, rf_rate=.01):
+def calcualte_performance(df, rf_rate=.01, isPrint=True):
   '''
   Calculate additional information of portfolio
   :param df: The dataframe with daily returns
@@ -177,19 +211,33 @@ def calcualte_performance(df, rf_rate=.01):
   rst['mdd'] = df['mdd'].min()
   rst['bm_mdd'] = df['bm_mdd'].min()
 
-  print('CAGR: {:.2%}'.format(rst['annual_rtn'] - 1))
-  print('Accumulated return: {:.2%}'.format(rst['acc_rtn'] - 1))
-  print('Average return: {:.2%}'.format(rst['avg_rtn'] - 1))
-  print('Benchmark return : {:.2%}'.format(rst['bm_rtn']-1))
-  print('Number of trades: {}'.format(rst['no_trades']))
-  print('Number of win: {}'.format(rst['no_win']))
-  print('Hit ratio: {:.2%}'.format(rst['hit_ratio']))
-  print('Investment period: {:.1f}yrs'.format(rst['period']/365))
-  print('Sharpe ratio: {:.2f}'.format(rst['sharpe_ratio']))
-  print('MDD: {:.2%}'.format(rst['mdd']-1))
-  print('Benchmark MDD: {:.2%}'.format(rst['bm_mdd']-1))
-  print("\n\n\n")
-  return
+  if isPrint :
+    print('CAGR: {:.2%}'.format(rst['annual_rtn'] - 1))
+    print('Accumulated return: {:.2%}'.format(rst['acc_rtn'] - 1))
+    print('Average return: {:.2%}'.format(rst['avg_rtn'] - 1))
+    print('Benchmark return : {:.2%}'.format(rst['bm_rtn']-1))
+    print('Number of trades: {}'.format(rst['no_trades']))
+    print('Number of win: {}'.format(rst['no_win']))
+    print('Hit ratio: {:.2%}'.format(rst['hit_ratio']))
+    print('Investment period: {:.1f}yrs'.format(rst['period']/365))
+    print('Sharpe ratio: {:.2f}'.format(rst['sharpe_ratio']))
+    print('MDD: {:.2%}'.format(rst['mdd']-1))
+    print('Benchmark MDD: {:.2%}'.format(rst['bm_mdd']-1))
+    print("\n\n\n")
+  ret ={}
+
+  ret['CAGR'] = float(round((rst['annual_rtn'] - 1)*100, 3))
+  ret['Accumulated'] = float(round((rst['acc_rtn'] - 1)*100, 3))
+  ret['Average'] =round( (rst['avg_rtn'] - 1)*100, 3)
+  ret['Benchmark'] =float(round( (rst['bm_rtn']-1)*100, 3))
+  ret['Trade'] = int(rst['no_trades'])
+  ret['Win'] = int(rst['no_win'])
+  ret['Hit ratio'] = float(round(rst['hit_ratio']*100))
+  ret['Investment period'] = int(rst['period'])
+  ret['Sharpe'] = float(round(rst['sharpe_ratio']*100,3))
+  ret['MDD'] = round((rst['mdd']-1)*100, 3)
+  ret['Benchmark MDD'] =round( (rst['bm_mdd']-1)*100, 3)
+  return ret
 
 
 def __get_period(df):
